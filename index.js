0ad3dd13069ec7cf1c9acafad66de9b843b0caa2
Sharmina Akhtar <sakht318@gmail.com>
2018-08-05 14:11:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0,2);
-}
+};
 
 const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-2);
-}
+};
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
@@ -14,7 +14,7 @@ const createFareMultiplier = function(int1) {
   return function(int2){
     return int1 * int2;
   };
-}
+};
 
 const fareDoubler = function (fare) {
   return createFareMultiplier(fare) * 2
