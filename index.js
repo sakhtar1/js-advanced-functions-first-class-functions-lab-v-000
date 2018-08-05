acc374f89974ec30c412ea42a0500330aaf4b126
Sharmina Akhtar <sakht318@gmail.com>
2018-08-05 12:53:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.first && drivers[1];
+}
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.last && drivers[-2];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
