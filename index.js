485c9c4d41ae4113e3c8f14544c1b53ff8697318
Sharmina Akhtar <sakht318@gmail.com>
2018-08-05 14:16:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,6 @@ const fareDoubler = createFareMultiplier(2);
 
 
 const fareTripler = createFareMultiplier(3);
-
-
+const selectDifferentDrivers = function(array, drivers) {
+  return drivers(array);
+}
