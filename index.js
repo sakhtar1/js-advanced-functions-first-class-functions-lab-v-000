498494ecb23b3c8d386021c91771bafdae9a33e6
Sharmina Akhtar <sakht318@gmail.com>
2018-08-05 14:08:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const returnLastTwoDrivers = function (drivers) {
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const createFareMultiplier = function (int) {
+function createFareMultiplier(int) {
   return int * 4;
+}
+
+const fareDoubler = function (fare) {
+  return createFareMultiplier(fare) * 2
 }
